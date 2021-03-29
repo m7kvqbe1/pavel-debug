7d3d29ad50180901238af53d093151e3cd10c574
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-03-29 12:41:20 +0100

    Pavel number input example

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '@royalnavy/css-framework/dist/styles.css'
+import { NumberInput } from '@royalnavy/react-component-library'
 
 import { Main } from '../../layouts'
 
-export const Home: React.FC<unknown> = () => (
-  <Main>
-    <h1>Hello, World!</h1>
-  </Main>
-)
+export const Home: React.FC<unknown> = () => {
+  const initial = {
+    foo: {
+      bar: 1,
+    },
+  }
+
+  const [state, setState] = useState(initial)
+
+  const onChangeHandler = (e: any) => {
+    const {
+      target: { value },
+    } = e
+
+    setState({ ...state, ...{ foo: { bar: value } } })
+
+    console.log('updated', state)
+  }
+
+  return (
+    <Main>
+      <NumberInput
+        canClear
+        name="number-input"
+        onChange={onChangeHandler}
+        value={1}
+      />
+    </Main>
+  )
+}
